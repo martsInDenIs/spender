c14d6f6246b34a45024306c962f983724aa0a05c
Денис Марцин <user@Deniss-Laptop.local>
2024-06-10 23:27:22 +0300

    feat: add basic navigation to the Header

--- app/dashboard/components/Header.tsx
+++ app/dashboard/components/Header.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header: React.FC = () => {
-    return <header className='h-12 bg-2 text-center text-lg flex items-center justify-center text-white shrink-0'>Header</header>
+    return <header className='h-12 bg-2 text-center text-lg flex items-center justify-center text-white shrink-0'>
+        <ul className='list-none flex gap-5 [&>li:hover]:underline'>
+            <li>
+                <Link href='/dashboard'>Dashboard</Link>
+            </li>
+            <li>
+                <Link href='/transactions'>Transactions</Link>
+            </li>
+        </ul>
+    </header>
 }
 
 export default Header;
