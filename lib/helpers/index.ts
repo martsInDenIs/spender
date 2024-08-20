fe2be133a7a6fc0426dba402543f838b9e8127b7
Denys Martsyn <aqezy21@gmail.com>
2024-08-20 15:25:02 +0300

    feat: put cookies inside of each request

--- lib/helpers/index.ts
+++ lib/helpers/index.ts
@@ -1,3 +1,5 @@
+import { cookies } from "next/headers";
+
 export const baseFetch = (
   path: Parameters<typeof fetch>[0],
   options?: RequestInit
@@ -9,23 +11,31 @@ export const baseFetch = (
 
   return fetch(
     `${baseApi}${path.toString().startsWith("/") ? path : `/${path}`}`,
-    options
+    {
+      ...options,
+      headers: {
+        ...options?.headers,
+        Cookie: cookies().toString(),
+      },
+    }
   );
 };
 
 export function formatParams(params: Record<string, unknown> | undefined) {
   const paramsString = params
-    ? Object.entries(params).map(([key, value]) => {
-        if (value === undefined || value === null || value === "") {
-          return "";
-        }
+    ? Object.entries(params)
+        .map(([key, value]) => {
+          if (value === undefined || value === null || value === "") {
+            return "";
+          }
 
-        if (Array.isArray(value)) {
-          return value.map((el) => `${key}[]=${el}`).join("&");
-        }
+          if (Array.isArray(value)) {
+            return value.map((el) => `${key}[]=${el}`).join("&");
+          }
 
-        return `${key}=${value}`;
-      }).join('&')
+          return `${key}=${value}`;
+        })
+        .join("&")
     : "";
 
   return paramsString.length ? `?${paramsString}` : "";
